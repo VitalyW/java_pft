f9b4b6cc5ce763b4518a4dcf9bba7d6a74834be4
VitalyW <vitzeev1@gmail.com>
2018-02-05 10:36:23 -0600

    Create issue via soap

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -24,6 +24,7 @@ public class ApplicationManager {
   private JamesHelper jamesHelper;
   private LoginHelper loginHelper;
   private PasswordResetHelper passwordResetHelper;
+  private SoapHelper soapHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -106,6 +107,13 @@ public class ApplicationManager {
       passwordResetHelper = new PasswordResetHelper(this);
     }
     return passwordResetHelper;
+  }
+
+  public SoapHelper soap() {
+    if (soapHelper == null) {
+      soapHelper = new SoapHelper(this);
+    }
+    return soapHelper;
   }
 
 }
