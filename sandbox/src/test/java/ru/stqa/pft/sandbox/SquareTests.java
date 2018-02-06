6c790cc8371e91005cf8e59a61303cd29d396210
VitalyW <vitzeev1@gmail.com>
2018-02-05 19:18:02 -0600

    Jenkins tests

--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -10,7 +10,7 @@ public class SquareTests {
   @Test
   public void testArea() {
     Square s = new Square(4);
-    Assert.assertEquals(s.area(), 16);
+    Assert.assertEquals(s.area(), 17);
 
   }
 }
